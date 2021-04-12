8f9254db35165740ceac451668245349f2588be7
Haoting Qiu <easonyau@126.com>
2021-04-12 16:37:10 -0400

    configure cors using cors package

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ const cors = require('cors')
 const corsOptions = {
     origin: 'https://cupola.herokuapp.com',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    methods: ['GET', 'PUT', 'POST', "DELETE", "OPTIONS"]
+    methods: ['GET', 'PUT', 'POST', "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "X-Requested-With", "Origin"]
 }
+
+app.use(cors(corsOptions))
 
 // confiugre middlewares
 // configure CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://cupola.herokuapp.com')
-    res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin')
-    res.header('Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-    next()
-})
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://cupola.herokuapp.com')
+//     res.header('Access-Control-Allow-Headers',
+//         'Content-Type, X-Requested-With, Origin')
+//     res.header('Access-Control-Allow-Methods',
+//         'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+//     next()
+// })
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
